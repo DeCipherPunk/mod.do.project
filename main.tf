72b4b5a01813f9b011d505323abe1abde1aef5db
DeCipherPunk <decipherpunk@protonmail.com>
2021-10-21 02:43:40 +0530

    define provider

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+ provider "digitalocean" {
+    token = "${var.do_token}"
+ }
 resource "digitalocean_project" "default" {
   name        = "${var.do_project_name}"
   description = "${var.do_project_description}"
